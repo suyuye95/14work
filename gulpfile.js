6401ccd0114be98b4e0ab4a36916e02eb35d3c3f
syy <446390252@qq.com>
2019-02-14 20:11:10 +0800

    全部完成提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var cleanCss = require('gulp-clean-css');
 gulp.task('minSass', function() {
     return gulp.src('./scss/*.scss')
         .pipe(sass())
+        .pipe(gulp.dest('./css/'))
+})
+gulp.task('minCss', function() {
+    return gulp.src('./css/*.css')
+        .pipe(concat('all.css'))
+        .pipe(cleanCss())
         .pipe(gulp.dest('./css/'))
 })
 gulp.task('watch', function() {
